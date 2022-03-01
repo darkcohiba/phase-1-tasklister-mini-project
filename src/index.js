3ae2af0fbc29b473ce4a77dd4aad53a0558dffc3
Sam <samuel.waters@gmail.com>
2022-02-28 22:15:27 -0700

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector('form')
+  const tasks = document.querySelector('#tasks')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleToDo(e.target.newTask.value);
+  })
+})
+function removeToDo (e){
+  e.target.parentNode.remove()
+}
+
+function handleToDo(todo){
+  let list = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.textContent = 'X'
+  btn.addEventListener('click', removeToDo)
+  list.textContent = `${todo} `
+  tasks.append(todo , btn)
+}
