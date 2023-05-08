6ae2b45b2ea72abb6fc24dc96947ec5f9bd93e79
Sam <samuel.waters@gmail.com>
2023-05-08 15:03:32 -0400

    chore: cody

--- src/index.js
+++ src/index.js
@@ -6,6 +6,29 @@ document.addEventListener("DOMContentLoaded", () => {
     handleToDo(e.target.newTask.value);
   })
 })
+
+function getWorkspaces(){
+  fetch("http://localhost:3000/workspaces")
+  .then(r => r.json())
+  .then(data => {
+    console.log("Data")
+    console.log(data) // all workspaces
+    // console.log(data.taskLister[1].tasks)
+    for (let workspace in data){ // workspace obj
+      console.log("Workspace")
+      console.log(data[workspace])
+      for (let list in workspace){ // workspace lists
+        console.log(workspace + " list")
+        console.log(data[workspace][list])
+        // for (let task in list){
+        //   console.log("task")
+        //   console.log(data[workspace][list])
+        // }
+      }
+    }
+  })
+}
+
 function removeToDo (e){
   e.target.parentNode.remove()
 }
